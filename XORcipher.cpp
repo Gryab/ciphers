a5e10236b39917f9b7607919f9a22bdc6fb54d6d
GRyab <ya.gleb.ryabtsev@gmail.com>
2022-07-30 00:42:51 +0300

    reverted to bitset for better readability

--- XORcipher.cpp
+++ XORcipher.cpp
@@ -1,20 +1,5 @@
 #include "XORcipher.h"
 
-cbitset cbitset::operator^ (cbitset another) { 
-	cbitset res; 
-	for (char i = -1; ++i < lastBitPos + 1;) res[i] = __super::at(i) ^ another.at(i);
-	return res; 
-}
-cbitset cbitset::operator^= (cbitset another) { 
-	__super::operator=(*this ^ another); 
-	return *this; 
-}
-cbitset::operator wchar_t() {
-	wchar_t res = __super::at(lastBitPos);
-	for (char i = -1; ++i < lastBitPos;) res += __super::at(i) << (lastBitPos - i);
-	return res;
-}
-
 GEX GEX::operator= (GEX another) { 
 	*this->Gvalue = another;
 	return *this; 
@@ -26,20 +11,20 @@ GEX GEX::operator^= (GEX another) {
 
 std::vector<GEX> XOR::XORstrToGexAsVec(cwstring phrase) {
 	std::vector<GEX> gex;
-	unsigned long long i = -1;
+	uint64_t i = -1;
 	while (++i < phrase.size()) gex.push_back(GEX(phrase.at(i)) ^ GEX(myKey.at(i)));
 	return gex;
 }
 std::wstring XOR::XORstrToGexAsStr(cwstring phrase) {
 	cwstring gex(gexIdentifier);
-	unsigned long long i = -1;
+	uint64_t i = -1;
 	std::vector<GEX> vec = XORstrToGexAsVec(phrase);
 	while (++i < phrase.size()) gex += (std::wstring)vec.at(i);
 	return (std::wstring)gex;
 }
 std::wstring XOR::XORgexToStr(cwstring phrase) {
 	cwstring str;
-	unsigned long long i = -1;
+	uint64_t i = -1;
 	while (++i < (phrase.size() >> 1)) str += (wchar_t)GEX(phrase.substr(i << 1, 2)) ^ GEX(myKey.at(i));
 	return (std::wstring)str;
 }
--- XORcipher.h
+++ XORcipher.h
@@ -1,31 +1,13 @@
 ﻿#pragma once
 #include <vector>
 #include <string>
-
-class cbitset : public std::vector<bool>
-{
-private:
-	const char lastBitPos = 15;
-public:
-	cbitset(wchar_t ch) : std::vector<bool>(16) { for (char i = -1; ++i < lastBitPos + 1; ch >>= 1) __super::operator[](lastBitPos - i) = ch % 2; }
-	cbitset(std::vector<bool> bin) : std::vector<bool>(16) { __super::assign(bin.begin(), bin.end()); }
-	cbitset() : std::vector<bool>() { __super::assign(16, 0); }
-
-	bool at(unsigned long long k) { return __super::at(k % __super::size()); }
-
-	cbitset operator^ (cbitset another);
-	cbitset operator^= (cbitset another);
-
-	operator wchar_t();
-
-	operator std::vector<bool>() { return std::vector<bool>(__super::begin(), __super::end()); }
-};
+#include <bitset>
 
 class GEX
 {
 private:
-	const unsigned short gex0 = 1024U;
-	const unsigned short gexBase = 256U;
+	const uint16_t gex0 = 1024U;
+	const uint16_t gexBase = 256U;
 	wchar_t Gvalue[3]{gex0, gex0};
 public:
 	GEX(wchar_t ch) { Gvalue[0] += ch % gexBase, Gvalue[1] += ch / gexBase; }
@@ -35,7 +17,7 @@ public:
 	operator wchar_t() { return (Gvalue[1] - gex0) * gexBase + Gvalue[0] - gex0; }
 
 	GEX operator= (GEX another);
-	GEX operator^ (GEX another) { return GEX(cbitset(*this) ^ cbitset(another)); }
+	GEX operator^ (GEX another) { return GEX((std::bitset<16>( *this) ^ std::bitset<16>(another)).to_ulong()); }
 	GEX operator^= (GEX another);
 };
 
@@ -44,9 +26,9 @@ class cwstring : public std::wstring
 public:
 	cwstring(std::wstring strT = L"") : std::wstring(strT) {};
 	cwstring(wchar_t wchar) : std::wstring(&wchar) {};
-	cwstring(std::vector<GEX> vec) { for (unsigned long long i = -1; ++i < vec.size();) __super::append(vec.at(i)); };
+	cwstring(std::vector<GEX> vec) { for (uint64_t i = -1; ++i < vec.size();) __super::append(vec.at(i)); };
 
-	wchar_t at(unsigned long long k) { return __super::at(k % __super::length()); }
+	wchar_t at(uint64_t k) { return __super::at(k % __super::length()); }
 };
 
 class XOR
@@ -58,7 +40,6 @@ public:
 	std::wstring cipher(std::wstring phrase) { return isGex(phrase) ? XORgexToStr(removeIdentifier(phrase)) : XORstrToGexAsStr(phrase); }
 	
 	std::vector<GEX> XORstrToGexAsVec(cwstring phrase);
-
 private:
 	std::wstring gexIdentifier = L"gexInput";
 	cwstring myKey;
